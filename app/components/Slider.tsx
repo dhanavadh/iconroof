47eee6fc56547d1eb128d1757f57d7d63d4427e2
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-01-29 14:44:49 +0700

    update slider option

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -6,6 +6,7 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import Link from 'next/link';
 
 const Slider = () => {
   return (
@@ -13,52 +14,60 @@ const Slider = () => {
         <Swiper        
         centeredSlides={true}
         autoplay={{
-          delay: 5000,
+          delay: 6000,
           disableOnInteraction: false,
         }}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper font-[family-name:var(--font-noto-sans)]"
       >
+
+        {/* Parent Please Clone From This */}
         <SwiperSlide>
-            <div className='min-h-screen flex items-end text-white'>
-                <img src='https://dhanav.me/banner/buddhism.svg' className='absolute z-[-1] w-full h-full object-cover brightness-50'></img>
-                <div className='p-10'>
-                    <p className='text-3xl'>ระแนง Amigo</p>
-                    <p>รายละเอียด</p>
+            <div className='slider-core'>             
+                <img src='/asset/scg.png' className='hidden lg:flex absolute z-[-1] w-full h-full object-cover brightness-50'></img>
+                <img src='/asset/scg-vertical.png' className='flex lg:hidden absolute z-[-1] w-full h-full object-cover brightness-50'></img>
+                <div className='text-area-padding'>
+                    <Link href='/products' className='title-section link'>ระแนงบังตาไวนิล SCG BOX SET</Link>
+                    <p className='subtitle-section'>สร้างสรรค์ความงามได้หลากหลาย ภายใต้คุณภาพที่เป็นมาตรฐาน</p>
                 </div>
             </div>
-        </SwiperSlide>            
+        </SwiperSlide> 
+        
         <SwiperSlide>
-            <div className='min-h-screen flex items-end text-white'>
-                <img src='https://dhanav.me/banner/blog-2.svg' className='absolute z-[-1] w-full h-full object-cover brightness-50'></img>
-                <div className='p-10'>
-                    <p className='text-3xl'>ระแนง Amigo</p>
-                    <p>รายละเอียด</p>
+            <div className='slider-core'>             
+                <img src='/asset/amigo.png' className='hidden lg:flex absolute z-[-1] w-full h-full object-cover brightness-50'></img>
+                <img src='/asset/amigo.png' className='flex lg:hidden absolute z-[-1] w-full h-full object-cover brightness-50'></img>
+                <div className='text-area-padding'>
+                    <Link href='/products' className='title-section link'>ระแนงไวนิล AMIGO</Link>
+                    <p className='subtitle-section'>ไม้สังเคราะห์ทำจากโพลิเมอร์ชนิดพิเศษทนทานจึงหมดปัญหาเรื่องปลวกและเชื้อราไม่ปิดตัว ไม่บวมน้ำ ทนต่อแสงแดด</p>
                 </div>
             </div>
-        </SwiperSlide>            
+        </SwiperSlide> 
+
         <SwiperSlide>
-            <div className='min-h-screen flex items-end text-white'>            
-                <img src='https://dhanav.me/banner/blog-1.svg' className='absolute z-[-1] w-full h-full object-cover brightness-50'></img>
-                <div className='p-10'>
-                    <p className='text-3xl'>ระแนง Amigo</p>
-                    <p>รายละเอียด</p>
+            <div className='slider-core'>             
+                <img src='/asset/greenwood1.png' className='flex absolute z-[-1] w-full h-full object-cover brightness-50'></img>                
+                <div className='text-area-padding'>
+                    <Link href='/products' className='title-section link'>ระแนง GREENWOOD</Link>
+                    <p className='subtitle-section'>ผลิตภัณฑ์ไม้สังเคราะห์ที่มีส่วนผสมของไม้ธรรมชาติ จึงให้ผิวสัมผัสและภาพลักษณ์เสมือนไม้จริง มากกว่าวัสดุทดแทนไม้ชนิดอื่นๆ</p>
                 </div>
             </div>
-        </SwiperSlide>            
+        </SwiperSlide> 
+
         <SwiperSlide>
-            <div className='min-h-screen flex items-end text-white'> 
-            <video width="full" autoPlay loop muted preload='auto' playsInline className='absolute z-[-1] w-full h-full object-cover brightness-50'>
-              <source src="https://dhanav.me/content/port-redesign/navbar.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-                {/* <img src='https://dhanav.me/banner/blog-1.svg' className='absolute z-[-1] w-full h-full object-cover brightness-50'></img> */}
-                <div className='p-10'>
-                    <p className='text-3xl'>ระแนง Amigo</p>
-                    <p>รายละเอียด</p>
+            <div className='slider-core'>             
+                <video width="full" autoPlay loop muted preload='auto' playsInline className='absolute z-[-1] w-full h-full object-cover brightness-50'>
+                    <source src="/video/icon-1.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <div className='text-area-padding'>
+                    <Link href='/products' className='title-section link'>ระแนงบังตาไวนิล SCG BOX SET</Link>
+                    <p className='subtitle-section'>สร้างสรรค์ความงามได้หลากหลาย ภายใต้คุณภาพที่เป็นมาตรฐาน</p>
                 </div>
             </div>
-        </SwiperSlide>            
+        </SwiperSlide> 
+
+        
       </Swiper>
     </>
   )
