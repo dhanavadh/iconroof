7bef6ddb803795e80b14031d6029228cc3085a5a
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-02-13 23:06:17 +0900

    add installation

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -2,7 +2,10 @@ import Link from 'next/link';
 import React from 'react'
 
 const services = [
-    { id:'1', name: 'ระแนงบังตาไวนิล SCG BOX SET', des: 'สร้างสรรค์ความงามได้หลากหลาย ภายใต้คุณภาพที่เป็นมาตรฐาน', image: '/asset/scg.png', link: '/product' },
+    { id:'1', name: 'ระแนงไวนิล SCG BOX SET', des: 'สร้างสรรค์ความงามได้หลากหลาย ภายใต้คุณภาพที่เป็นมาตรฐาน', image: '/asset/scg.png', link: '/product' },
+    { id:'2', name: 'ระแนงไวนิล AMIGO', des: 'ไม้สังเคราะห์ทำจากโพลิเมอร์ชนิดพิเศษทนทานจึงหมดปัญหาเรื่องปลวกและเชื้อราไม่ปิดตัว ไม่บวมน้ำ ทนต่อแสงแดด', image: '/asset/image.png', link: '/product' },
+    { id:'3', name: 'ระแนง GREENWOOD', des: 'ผลิตภัณฑ์ไม้สังเคราะห์ที่มีส่วนผสมของไม้ธรรมชาติ จึงให้ผิวสัมผัสและภาพลักษณ์เสมือนไม้จริง มากกว่าวัสดุทดแทนไม้ชนิดอื่นๆ', image: '/asset/greenwood.png', link: '/product' },
+    { id:'4', name: 'ฝ้าระแนง', des: 'ฝ้าระแนงลายไม้ wpc ไฟเบอร์ซีเมนต์ ไวนิล Amigo หรือ อะลูมิเนียมลายไม้ก็มีพร้อมให้เลือกสรร', image: '/asset/roof-1.png', link: '/product' },    
     
 ]
 
@@ -14,14 +17,14 @@ const ProductsList = () => {
                 <ul className='flex'>
                     {services.map((item) => (
                         <li key={item.id}>
-                            <div className='grid w-96 items-center mx-3 my-10 bg-gray-50 rounded-lg shadow-xl duration-300'>
+                            <div className='grid w-72 md:w-96 items-center mx-2 my-10 bg-gray-50 rounded-lg shadow-xl duration-300'>
                                 <div className='relative h-64'>
                                     <img src={item.image} className='w-full h-full object-cover absolute rounded-t-lg' alt={item.name}></img>
                                 </div>
                                 <div className='flex flex-col p-6 whitespace-normal'>
                                     <div>
                                         <p className='h3'>{item.name}</p>
-                                        <p className='p'>{item.des}</p>
+                                        <p className='p line-clamp-2'>{item.des}</p>
                                     </div>                                        
                                     <Link href={item.link} className="text-end mt-4 link">ดูสินค้า →</Link>                              
                                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Slider from "./components/Slider";
 import Alert from "./components/Alert";
 import Link from "next/link";
 import ProductsList from "./components/ProductList";
+import InstallationList from "./components/Install";
 
 export default function Home() {
   return (
@@ -13,26 +14,17 @@ export default function Home() {
     <>
     <Slider />
     <Alert />
-    <div className="flex flex-col min-h-screen items-start mx-10 md:mx-20 font-[family-name:var(--font-noto-sans)]">      
-      <div className="flex flex-col items-start pt-4 ">
+    <div className="flex flex-col min-h-screen items-start mx-6 md:mx-20 font-[family-name:var(--font-noto-sans)]">      
+      <div className="flex flex-col items-start pt-8 ">
         <p className="h1">เลือกดูผลิตภัณฑ์ระแนง</p>
         <p className="p">ศูนย์รวมระแนง ไม้ระแนงไวนิล AMIGO ระแนงบังตา และช่างทำระแนง โดย Iconroof</p>        
       </div> 
       <ProductsList /> 
-        <div className="w-96 h-1/2 shadow-lg rounded-lg my-4">
-          {/* Image Container */}
-          {/* <div className="flex relative w-full h-96 items-end">
-              <img src="/asset/scg.png" className="flex absolute z-[-1] w-full h-full object-cover"></img>
-              <p className="px-4 py-2 h1 text-neutral-50">ระแนงบังตาไวนิล SCG BOX SET</p>
-          </div> */}
-          {/* Text Source */}
-          <img src="/asset/scg.png" className="flex w-full h-full"></img>            
-          <div className="flex flex-col bg-white px-4 py-2 w-full">
-              <p className="h3">ระแนงบังตาไวนิล SCG BOX SET</p>
-              <p className="p">สร้างสรรค์ความงามได้หลากหลาย ภายใต้คุณภาพที่เป็นมาตรฐาน</p>
-              <Link href="/products" className="text-end my-4 link">ดูสินค้า →</Link>
-          </div>  
-        </div> 
+      <div className="flex flex-col items-start pt-4 ">
+        <p className="h1">งานติดตั้งระแนง</p>
+        <p className="p">ศูนย์รวมระแนง ไม้ระแนงไวนิล AMIGO ระแนงบังตา และช่างทำระแนง โดย Iconroof</p>        
+      </div> 
+      <InstallationList />
     </div>
     
     </>
