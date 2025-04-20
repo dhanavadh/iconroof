81d7e5db6d1a01d9934c4e688ff3e47dafe0c259
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-04-20 17:01:35 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className='flex min-h-screen items-center justify-center w-full'>
+    <div className='flex min-h-screen items-center justify-center w-full px-6 md:px-20'>
       <div className=''>
         <div className='mb-8'>
             <h2 className='h1'>ไม่พบหน้าที่กำลังหา</h2>
-            <p className='h4'>หน้าที่กำลังหาอาจอยู่ในระหว่างการปรับปรุง โปรดลองใหม่อีกครั้ง</p>
+            <p className='h4'>หน้าที่กำลังหาอาจอยู่ในระหว่างการปรับปรุง</p>
         </div>
         <Link href="/" className=' px-4 py-2 rounded-full border-2 border-neutral-800 bg-neutral-800 text-white font-medium hover:bg-neutral-600 hover:border-neutral-600 cursor-pointer transition-colors duration-300 font-[family-name:var(--font-noto-sans)]'>กลับหน้าแรก</Link>
       </div>
